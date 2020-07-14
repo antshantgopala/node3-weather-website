5a6bee712f949bf5a5b3c7722dcdd1a1ec0fc4cd
Pranav Gopal <pranav14sharma@gmail.com>
2020-07-14 12:30:31 +0530

    Init Commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request')
+
+const forecast = (lang, lati, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=702c8a0f1ab35d7471bc1c21f93679d3&query=' + encodeURIComponent(lang) +','+ encodeURIComponent(lati) + '&units=m'
+    request({ url, json: true }, (error, { body }) =>{
+        if(error) {
+            callback('Unable to connect to internet', undefined)
+        } else if(body.error) {
+            callback('Unable to find location for given geocode', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently '+body.current.temperature+' degrees out and it feels like ' + body.current.feelslike + ' degrees')
+        }
+    })
+}
+
+module.exports = forecast
