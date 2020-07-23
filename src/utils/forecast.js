9fd731b1eafecf54890b8ac5d55fc355226520c8
Pranav Gopal <pranav14sharma@gmail.com>
2020-07-23 13:13:43 +0530

    Humidity added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (lang, lati, callback) => {
         } else if(body.error) {
             callback('Unable to find location for given geocode', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently '+body.current.temperature+' degrees out and it feels like ' + body.current.feelslike + ' degrees')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently '+body.current.temperature+' degrees out and it feels like ' + body.current.feelslike + ' degrees. The humidity is ' + body.current.humidity + '%.')
         }
     })
 }
